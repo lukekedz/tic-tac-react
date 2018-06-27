31da05ce35092d3cc3ffcb7ca2c1d79818561a58
Luke Kedz <luke.kedziora@jefferson.edu>
2018-06-27 13:56:18 -0400

    removing unused code

--- src/index.js
+++ src/index.js
@@ -2,22 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
-// class Square extends React.Component {
-//   render() {
-//     return (
-//       <button 
-//         className="square" 
-//         onClick={() => this.props.onClick() }
-//       >
-//         {this.props.value}
-//       </button>
-//     )
-//   }
-// }
-
-// class/component Square as a FUNCTIONAL COMPONENT
 function Square(props) {
-  //  tutorial note: In a class, we used an arrow function to access the correct this.value, but in a functional component we don’t need to worry about this.
   return (
     <button className="square" onClick={props.onClick}>
       {props.value}
@@ -25,73 +10,6 @@ function Square(props) {
   )
 }
 
-// tutorial note: WITH NO HISTORY
-// class Board extends React.Component {
-//   constructor(props) {
-//     super(props)
-//     this.state = {
-//       squares: Array(9).fill(null),
-//       xIsNext: true,
-//     }
-//   }
-
-//   handleClick(i) {
-//     const squares = this.state.squares.slice()
-    
-//     if (calculateWinner(squares) || squares[i]) {
-//       return
-//     }
-
-//     squares[i] = this.state.xIsNext ? 'X' : 'O'
-//     this.setState({
-//       squares: squares,
-//       xIsNext: !this.state.xIsNext,
-//     })
-//   }
-
-//   renderSquare(i) {
-//     return (
-//       <Square 
-//         value={this.state.squares[i]} 
-//         onClick={() => this.handleClick(i)}
-//       />
-//     )
-//   }
-
-//   render() {
-//     // const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
-//     const winner = calculateWinner(this.state.squares)
-//     let status
-//     if (winner) {
-//       status = 'Winner: ' + winner
-//     } else {
-//       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
-//     }
-
-//     return (
-//       <div>
-//         <div className="status">{status}</div>
-//         <div className="board-row">
-//           {this.renderSquare(0)}
-//           {this.renderSquare(1)}
-//           {this.renderSquare(2)}
-//         </div>
-//         <div className="board-row">
-//           {this.renderSquare(3)}
-//           {this.renderSquare(4)}
-//           {this.renderSquare(5)}
-//         </div>
-//         <div className="board-row">
-//           {this.renderSquare(6)}
-//           {this.renderSquare(7)}
-//           {this.renderSquare(8)}
-//         </div>
-//       </div>
-//     )
-//   }
-// }
-
-// tutorial note: WITH HISTORY and functionality moved upward to Game
 class Board extends React.Component {
   renderSquare(i) {
     return (
