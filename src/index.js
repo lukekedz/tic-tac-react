9d8ad562edae006b4008d4fd11dbe604f01c04ba
Luke Kedz <luke.kedziora@jefferson.edu>
2018-06-29 09:12:51 -0400

    refactored switch statements

--- src/index.js
+++ src/index.js
@@ -50,23 +50,17 @@ class Game extends React.Component {
   convertArrayIndexToRow(position) {
     switch(position) {
       case 0:
-        return 1
       case 1:
-        return 1
       case 2:
         return 1
 
       case 3:
-        return 2
       case 4:
-        return 2
       case 5:
         return 2
 
       case 6:
-        return 3
       case 7:
-        return 3
       case 8:
         return 3
     }
@@ -75,23 +69,17 @@ class Game extends React.Component {
   convertArrayIndexToColumn(position) {
     switch(position) {
       case 0:
-        return 1
       case 3:
-        return 1
       case 6:
         return 1
 
       case 1:
-        return 2
       case 4:
-        return 2
       case 7:
         return 2
 
       case 2:
-        return 3
       case 5:
-        return 3
       case 8:
         return 3
     }
